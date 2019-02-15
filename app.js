9b9fc90a963991a90f2707411e74a7b54adc4187
zroyer <zroyer@utexas.edu>
2019-02-15 16:38:54 -0600

    fun tweets

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Twit = require('twit');
 const TwitterBot = require('node-twitterbot').TwitterBot;
 var randomEmoji = require('random-emoji');
+var html2unicode = require("html2unicode");
 
 const emojiHaikuBot = new TwitterBot({
   consumer_key: 'u1IbPHqvfJfvXvm8p2UW4aSw4',
@@ -9,21 +10,38 @@ const emojiHaikuBot = new TwitterBot({
   access_token_secret: 'AXSf1V25RA4LyjR8gtvp6ac1aiaXbW18cgRQm1IfjgFHN'
 });
 
-function formatTranslation(str, find, replace) {
-    return str.replace(new RegExp(find, 'g'), replace).replace(/[0-9]/g, '');
-}
-
 function getEmojiLine(array) {
-  console.log(array);
   return array.map(emoji => emoji.character).join(' ');
 }
 
-function getTranslationLine(array) {
-  return formatTranslation(array.map(emoji => emoji.name).join(' '), '_', '-');
+async function getTranslationLine(array) {
+  let htmlLine = (
+    '<em><b>' +
+    formatTranslation(array.map(emoji => emoji.name).join(' '), '_', '-') +
+    '</b></em>'
+  );
+  return await html2unicode.html2unicode(htmlLine);
+}
+
+function formatTranslation(str, find, replace) {
+  return str.replace(new RegExp(find, 'g'), replace).replace(/[0-9]/g, '');
+}
+
+async function tweet(haiku) {
+  const t = (
+    getEmojiLine(newHaiku[0]) + '\n' +
+    getEmojiLine(newHaiku[1]) + '\n' +
+    getEmojiLine(newHaiku[2]) + '\n\n' +
+    await getTranslationLine(newHaiku[0]) + '\n' +
+    await getTranslationLine(newHaiku[1]) + '\n' +
+    await getTranslationLine(newHaiku[2])
+  );
+
+  emojiHaikuBot.tweet(t);
 }
 
 const newHaiku = randomEmoji.haiku();
-const packagedTweet = getEmojiLine(newHaiku[0]) + '\n' + getEmojiLine(newHaiku[1]) + '\n' + getEmojiLine(newHaiku[2])
-  + '\n\n' + getTranslationLine(newHaiku[0]) + '\n' + getTranslationLine(newHaiku[1]) + '\n' + getTranslationLine(newHaiku[2]);
-
-emojiHaikuBot.tweet(packagedTweet);
+tweet(newHaiku)
+  .then(() => {
+    console.log('🆒');
+  });
---
+++ node_modules/saxophone/lib/index.js
@@ -0,0 +1,7 @@
+const Saxophone = require('./Saxophone');
+
+Saxophone.parseAttrs = require('./static/attrs');
+Saxophone.parseEntities = require('./static/entities');
+
+module.exports = Saxophone;
+
