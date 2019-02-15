38b5178f766f93a750f95f37009bfdd34efab813
zroyer <zroyer@utexas.edu>
2019-02-15 15:28:53 -0600

    change env var names

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const Twit = require(‘twit’);
 const TwitterBot = require(‘node-twitterbot’).TwitterBot;
 const emojiHaikuBot = new TwitterBot({
- consumer_key: process.env.BOT_CONSUMER_KEY,
- consumer_secret: process.env.BOT_CONSUMER_SECRET,
- access_token: process.env.BOT_ACCESS_TOKEN,
- access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+ consumer_key: process.env.TWITTER_CONSUMER_KEY,
+ consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+ access_token: process.env.TWITTER_ACCESS_TOKEN,
+ access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 emojiHaikuBot.tweet('hello world!');
